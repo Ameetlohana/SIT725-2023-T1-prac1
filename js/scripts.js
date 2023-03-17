af67553588a975171c0d1e11a2747375c96b5755
Ameetlohana <127467796+Ameetlohana@users.noreply.github.com>
2023-03-17 14:49:16 +1100

    I am resubmitting this task
    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 function changeText(){
-    document.getElementById("changetext").innerHTML = "This a changed text";
+    var textArray = ["a","b", "c","d", "e", "f"];
+    var number = getRandomNumberBetween(0,textArray.length - 1);
+    console.log("Index:", number);
+    document.getElementById("heading").innerHTML = textArray[number];
+}
+
+
+
+
+
+function getRandomNumberBetween(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
 }
